e265fbb46eb9f313b4e2cd1d9a1c695f816a13bc
huangbo <515870153@qq.com>
2024-11-29 20:54:05 +0800

    feat (插件)： 安装ui库、router等插件

--- src/main.js
+++ src/main.js
@@ -2,4 +2,26 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+import router from './router'
+
+const app = createApp(App)
+
+//注册ElementPlus图标组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+//使用ElementPlus
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+//路由
+app.use(router)
+app.mount('#app')
+
+
+
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          $primary-color: #ff0000;  // 直接定义变量
+          @import "@/styles/variables.scss";  // 或者引入文件
+        `
+      }
+    }
+  }
 })
